b365908483448342049d8f1e4950758b97ef4503
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2022-06-26 00:22:49 +0200

    lang att added, title tag moved to app.js

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        const lang = "en";
+
+        return (
+            <Html lang={lang}>
+                <Head>
+                    <meta name="author" content="Tina Mary Holdcroft" />
+                </Head>
+
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
