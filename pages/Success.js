6e1225bdec1c60b5f91b5f30c1117794b954d69c
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2022-06-26 15:48:03 +0200

    style adjustments

--- pages/Success.js
+++ pages/Success.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-export const Success = (props) => (
-    <div className='success-page'>
-        <h1>Order Confirmation</h1>
-        <ul>
-            <li><b>Name:</b> {props.firstName} {props.lastName}</li>
-            <li><b>Amount:</b> {props.amount}</li>
-            <li><b>Type: </b>{props.type}</li>
-            <li><b>Email: </b>{props.email}</li>
-            <li><b>Phone:</b> {props.phone}</li>
-            <li><b>Time:</b> {props.arrival} - {props.departure}</li>
-            <li><b>Comment: </b>{props.comment}</li>
-        </ul>
-    </div>
-)
+export function Success(props) {
+
+    return (
+        <div className='success-page'>
+            <h1>Order Confirmation</h1>
+            <ul>
+                <li><b>Name:</b> {props.firstName} {props.lastName}</li>
+                <li><b>Guests:</b> {props.amount}</li>
+                <li><b>Type: </b>{props.type}</li>
+                <li><b>E-mail: </b>{props.email}</li>
+                <li><b>Phone:</b> {props.phone}</li>
+                <li><b>Time:</b> {props.arrival} - {props.departure}</li>
+                <li><b>Comment: </b>{props.comment}</li>
+            </ul>
+        </div>
+    );
+}
 
 export default Success;
